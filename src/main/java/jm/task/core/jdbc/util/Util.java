564c777a045b9f6be677cbde2e9d78d7497c1494
mironovoc <mironbrrr1994@gmail.com>
2024-11-09 22:34:41 +0300

    настроил соединение

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,16 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    public static Connection getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection(
+                "jdbc:mysql://localhost:3306/kata1",
+                "root",
+                "90tulugaA."
+        );
+        return conn;
+    }
 }
